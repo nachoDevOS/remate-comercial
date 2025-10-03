3ff8680a8dca1286698ac2d235a1409aa03a08f1
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2025-10-03 12:52:25 -0400

    dev

--- resources/views/board/board-user.blade.php
+++ resources/views/board/board-user.blade.php
@@ -166,7 +166,7 @@
     <script src="{{ url('js/main.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.4.0/socket.io.js" integrity="sha512-nYuHvSAhY5lFZ4ixSViOwsEKFvlxHMU2NHts1ILuJgOS6ptUmAGt/0i5czIgMOahKZ6JN84YFDA+mCdky7dD8A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
-        const socket = io("localhost:3002");
+        const socket = io("rematecomercial-server.soluciondigital.dev");
     </script>
     <script>
         $(document).ready(function() {
